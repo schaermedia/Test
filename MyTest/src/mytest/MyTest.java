af2c2681f672743e4fdc7807b9542ca5d7bec188
quentin <quentin@pc4.fritz.box>
2016-01-10 14:39:40 +0100

    Test

--- MyTest/src/mytest/MyTest.java
+++ MyTest/src/mytest/MyTest.java
@@ -16,6 +16,10 @@ public class MyTest {
      */
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
     }
 
 }
