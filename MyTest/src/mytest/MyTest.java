a764d4c7c651e8f2cc991d4fcde0cefcdcfe8922
quentin <quentin@pc4.fritz.box>
2016-01-10 14:27:14 +0100

    Test

--- MyTest/src/mytest/MyTest.java
+++ MyTest/src/mytest/MyTest.java
@@ -15,7 +15,7 @@ public class MyTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hi");
     }
 
 }
